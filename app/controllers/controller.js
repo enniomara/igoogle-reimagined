fa85f5d6f19adc3285cbadff03af70b52abe7171
enniomara <enniomara1@gmail.com>
2015-04-22 21:59:17 +0200

    Added controller for loading the stock data via yahoo API.

--- app/controllers/controller.js
+++ app/controllers/controller.js
@@ -15,6 +15,45 @@ app.config(['$routeProvider',function($routeProvider) {
 	
 }]);
 
+app.controller('StockCtrl', function($scope, $http){
+	// Is used in select query for API
+	var stockCompanies = [
+		"GE",
+		"MSFT",
+		"GOOGL",
+		"AAPL",
+		"MYL",
+		"BAC",
+		"VZ",
+		"FB",
+		"RF"
+	];
+
+	$scope.stockValues = [{}];
+
+	// Thanks to YQL console(http://developer.yahoo.com/yql/console/?q=select%20Symbol%2CChange%2COpen%20from%20yahoo.finance.quoteslist%20where%20symbol%20%3D%20%22GOOGL%22&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys)
+	var financeAPI = "https://query.yahooapis.com/v1/public/yql?q=select%20Symbol%2CChange%2COpen%20from%20yahoo.finance.quoteslist%20where%20symbol%20in%20(%22"+ encodeURIComponent(stockCompanies.toString()) +"%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=";
+
+	// "https://query.yahooapis.com/v1/public/yql?q=select Symbol,Change,Open from yahoo.finance.quoteslist where symbol in (\""+ stockCompanies.toString() +"\")&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys&callback=";
+	$http.get(financeAPI)
+		.then(function(response) {
+			$scope.stockValues = response.data.query.results.quote;
+			console.log($scope.stockValues);
+		});
+
+	$scope.stockChangeColor = function(value){
+		value = parseFloat(value);
+		if(value < 0){
+			return "stock-negative";
+		}
+		else if(value === 0){
+			return "stock-neutral";
+		}
+		else if(value > 0){
+			return "stock-positive";
+		}
+	}
+});
 // Controller for handling the clicks on cards/in-card-currency 
 app.controller('ManageCardPageCtrl', ['', function($scope){
 	
