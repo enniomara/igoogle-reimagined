8a9c9d00701b57fab6dffe57b9c88fc741b3f1a1
enniomara <enniomara1@gmail.com>
2015-05-14 16:53:40 +0200

    Decided to remove results in the page(there are no proper APIs without authentication). Now it will redirect to duckduckgo.

--- app/controllers/controller.js
+++ app/controllers/controller.js
@@ -8,19 +8,22 @@
 var app = angular.module('searchResults', ['ngRoute']);
 
 
-app.config(['$routeProvider',function($routeProvider) {
+app.config(function($routeProvider) {
 	$routeProvider.
 		when('/', {
 			templateUrl: 'app/partials/iGoogleCards.html',
 			controller: 'ManageCardPageCtrl'
-		})
-		.when('/search', {
-			templateUrl: 'app/partials/searchResults.html',
-			controller: 'ListSearchResultsCtrl'
-		});
-	
-	
-}]);
+		}).
+		otherwise('/');
+});
+
+
+app.controller('SearchCtrl', function($scope, $window){
+	$scope.onFormSubmit = function(){
+		// console.log(searchInput)
+		$window.location.href = "https://duckduckgo.com/" + document.querySelector("input#searchTerm").value;
+	};
+});
 
 // Main controller that stores all variables used by the childcontrollers
 app.controller('MainCtrl', function($scope){
@@ -42,13 +45,6 @@ app.controller('MainCtrl', function($scope){
 			localStorage.setItem(currentValue, "");
 		}
 	});
-
-
-
-	// The search keyword that the user enters
-	$scope.searchInput = "";
-
-
 });
 
 app.controller('WeatherCtrl', function($scope, $http){
@@ -506,51 +502,4 @@ app.controller('CurrencyConversionCtrl', function($scope, $http){
 // Controller for handling the clicks on cards/in-card-currency
 app.controller('ManageCardPageCtrl', function($scope){
 
-});
-
-// Controller for listing search results
-app.controller('ListSearchResultsCtrl', function($scope){
-	$scope.results = [{
-		'search-results': [
-			{
-				'name': 'Why is the sky blue? :: NASA\'s The Space Place',
-				'url': 'spaceplane.nasa.gov/blue-sky',
-				'description': 'Blue light is scattered in all directions by the tiny molecules of air in Earth\'s atmosphere. Blue is scattered more than other colors because it travels as shorter, smaller waves. This is why we see a blue sky most of the time.'
-			},
-			{
-				'name': 'Why is the sky blue? :: NASA\'s The Space Place',
-				'url': 'spaceplane.nasa.gov/blue-sky',
-				'description': 'Blue light is scattered in all directions by the tiny molecules of air in Earth\'s atmosphere. Blue is scattered more than other colors because it travels as shorter, smaller waves. This is why we see a blue sky most of the time.'
-			},
-			{
-				'name': 'Why is the sky blue? :: NASA\'s The Space Place',
-				'url': 'spaceplane.nasa.gov/blue-sky',
-				'description': 'Blue light is scattered in all directions by the tiny molecules of air in Earth\'s atmosphere. Blue is scattered more than other colors because it travels as shorter, smaller waves. This is why we see a blue sky most of the time.'
-			},
-			{
-				'name': 'Why is the sky blue? :: NASA\'s The Space Place',
-				'url': 'spaceplane.nasa.gov/blue-sky',
-				'description': 'Blue light is scattered in all directions by the tiny molecules of air in Earth\'s atmosphere. Blue is scattered more than other colors because it travels as shorter, smaller waves. This is why we see a blue sky most of the time.'
-			}
-
-		],
-		'search-results-images': [
-			{
-				'src': 'assets/images/search-results/image-results/result-1.png',
-				'alt': 'Resultat 1'
-			},
-			{
-				'src': 'assets/images/search-results/image-results/result-2.png',
-				'alt': 'Resultat 2'
-			},
-			{
-				'src': 'assets/images/search-results/image-results/result-3.png',
-				'alt': 'Resultat 3'
-			},
-			{
-				'src': 'assets/images/search-results/image-results/result-4.png',
-				'alt': 'Resultat 4'
-			}
-		]
-	}];
 });
